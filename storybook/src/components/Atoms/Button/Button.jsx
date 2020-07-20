e8e3f3a4a0e793d762bfa55c775f8676b928f2ef
Ivelin Iliev <ivelin.iliev@global.com>
2020-07-20 08:03:32 +0100

    new: secondary button

--- storybook/src/components/Atoms/Button/Button.jsx
+++ storybook/src/components/Atoms/Button/Button.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import SVG from './../SVG/SVG';
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 import './style.scss';
 
-export const Button = ({children, icon, type}) => {
+export const Button = ({children, icon, type, ...rest}) => {
     return (
-        <div className={`btn btn--${type}`}>
+        <div className={`btn btn--${type}`} {...rest}>
             { type === 'main' ? 
                 <svg className="btn--main__border">
                     <rect x="0" y="0" fill="none" width="100%" height="100%"></rect>
                 </svg>
             : null }
-            {children}
+            <span>{children}</span>
             { icon ? <SVG className="btn__icon" icon={icon} /> : null}
         </div>
     )
 }
 
-Button.PropTypes = {
-    type: PropTypes.string,
-    children: PropTypes.object,
+Button.propTypes = {
+    type: propTypes.string,
+    children: propTypes.string,
 }
 
 Button.defautProps = {
--- storybook/src/components/Atoms/Button/stories.js
+++ storybook/src/components/Atoms/Button/stories.js
@@ -8,4 +8,9 @@ export const TextButton = () => <Button type="main">Test Worked</Button>
 
 export const IconButton = () => <Button type="main" icon="download">Test Worked</Button>
 
+export const SecondaryButton = () => <Button type="secondary">Test Worked</Button>
+
+export const SecondaryButtonWithIcon = () => <Button type="secondary" icon="contact">Test Worked</Button>
+
+
 export default { title: 'Atoms/Button' }
