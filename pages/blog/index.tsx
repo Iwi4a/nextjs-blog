4383f0b7fe2154567a15c5dff5e5a74db256cbcb
Ivelin Iliev <ivelin.iliev@global.com>
2020-08-22 17:28:33 +0100

    new: Blog page header

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react';
+import {FixedBackground} from '../../storybook';
+import { getBlogpageData } from '../../lib/api';
+import style from 'styled-components';
 
-const Blog = () => {
-return (
-        <div>
-            Blog page
-        </div>
-    )
+
+export async function getStaticProps() {
+    const res = await getBlogpageData();
+    return {
+        props: {
+            ...res.data
+        }
+    }
+}
+
+
+class Blog extends Component {
+    render() {
+        return (
+            <>
+                <FixedBackground
+                    image={this.props.page.acfHeader.headerImage.mediaItemUrl}
+                    style={{
+                        backgroundSize: "auto",
+                    }}
+                    height="60vh" />
+            </>
+        )
+    }
 }
 
 export default Blog;
